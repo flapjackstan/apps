509fd85ed74ef7757d0c1042ff2f1fff122c50b9
flapjackstan <elm.s.camargo@gmail.com>
2024-08-11 20:21:34 -0700

    updated general repo structure

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ Collection of apps that I use for different purposes and have saved as an `alias
 
 `dates` - Date calculator with two uses. 1) Provided a spcific date calculate the date ahead or back n days. 2) Calculate the days between 2 dates. Used this as like a reminder of how many days until the next holiday and little stuff like that.
 
-`names` - Generates a random name from an sqlite db dependent on the theme you pass it (simpsons|rappers). I use this to demo some random examples of processes. 
+`names` - Generates a random name from an sqlite db dependent on the theme you pass it (simpsons|rappers). I use this to demo some random examples of processes.
+
+`holiday` - Print the closest federal holiday from today.
+
--- holiday.py
+++ holiday.py
@@ -1,13 +1,14 @@
 import holidays
 from datetime import datetime
 
+
 def get_next_federal_holiday():
     """
-    Retrieve the next federal holiday in the US, its day of the week, 
+    Retrieve the next federal holiday in the US, its day of the week,
     and the number of days until that date.
 
     Returns:
-        str: The next federal holiday date, name, day of the week, 
+        str: The next federal holiday date, name, day of the week,
              and the number of days until the holiday.
     """
     # Get US federal holidays for the current year
@@ -20,20 +21,24 @@ def get_next_federal_holiday():
     next_holiday = min(date for date in us_holidays.keys() if date > today)
 
     # Get the day of the week
-    day_of_week = next_holiday.strftime('%A')
+    day_of_week = next_holiday.strftime("%A")
 
     # Calculate the number of days until the next holiday
     days_until_holiday = (next_holiday - today).days
 
-    return (f"The next federal holiday is {next_holiday} ({day_of_week}) - "
-            f"{us_holidays[next_holiday]}. It is in {days_until_holiday} days.")
+    return (
+        f"The next federal holiday is {next_holiday} ({day_of_week}) - "
+        f"{us_holidays[next_holiday]}. It is in {days_until_holiday} days."
+    )
+
 
 def main():
     """
-    Main function to print the next federal holiday, its day of the week, 
+    Main function to print the next federal holiday, its day of the week,
     and the number of days until that date.
     """
     print(get_next_federal_holiday())
+
 
 if __name__ == "__main__":
     main()

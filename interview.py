4048f5d7ea772b0c7c5fe152b47b9aaddbeede3d
flapjackstan <elm.s.camargo@gmail.com>
2025-01-07 08:26:15 -0800

    interview buddy app

--- interview.py
+++ interview.py
@@ -1,11 +1,17 @@
+"""Interview with a friend."""
+
 import argparse
 import random
+
+from bs4 import BeautifulSoup
+
 
 def parse_args() -> argparse.Namespace:
     """
     Parse command-line arguments.
 
-    Returns:
+    Returns
+    -------
         argparse.Namespace: Parsed arguments.
     """
     parser = argparse.ArgumentParser(
@@ -16,11 +22,18 @@ def parse_args() -> argparse.Namespace:
 
     # Define arguments (including the new interactive mode flag)
     parser.add_argument("--version", action="version", version="1.0")
-    parser.add_argument("-d", "--difficulty", choices=["E", "M", "H", "R"], help="Options: E for easy, M for medium, H for hard, R for random")
+    parser.add_argument(
+        "-d",
+        "--difficulty",
+        choices=["E", "M", "H", "R"],
+        help="Options: E for easy, M for medium, H for hard, R for random",
+    )
 
     return parser.parse_args()
 
+
 def main():
+    """Run script."""
     args = parse_args()
 
     difficulty = args.difficulty
@@ -35,11 +48,37 @@ def main():
     if difficulty == "H":
         difficulty = "hard"
 
-    print("Behavioral Prompt: You are a non technical hiring manager. Please give me a random behavioral interview question.")
+    print(
+        "Behavioral Prompt: You are a non-technical hiring manager. Please give me a random behavioral interview question."  #  noqa: E501
+    )
     print()
-    print(f"Technical Question Prompt: You are an experienced data scientist. Please give me a {difficulty} difficulty interview question. Also give me what you would expect as a good answer.")
+    print(
+        f"Technical Question Prompt: You are an experienced data scientist. Please give me a {difficulty} difficulty interview question. Also give me what you would expect as a good answer."  #  noqa: E501
+    )
     print()
-    print(f"Leetcode Question: Please walk through question {random.randint(1, 50)}")
+
+    # copied and pasted html and then formatted here
+    # https://www.freeformatter.com/html-formatter.html
+    # not sure why but requests wasnt working for me
+    with open("./assets/formatted_leetcode.html") as file:
+        html_content = file.read()
+
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    problem_containers = soup.find_all(
+        "div",
+        class_="flex flex-col border-b-[1.5px] duration-300 last:border-b-0 border-lc-fill-02 dark:border-dark-lc-fill-02 hover:bg-lc-fill-02 dark:hover:bg-dark-lc-fill-02 cursor-pointer",  #  noqa: E501
+    )
+
+    problem_dict = {}
+    for i, container in enumerate(problem_containers, start=1):
+        title_div = container.find("div", class_="truncate")
+        title = title_div.text if title_div else "No title found"
+
+        problem_dict[i] = {"Title": title}
+
+    print(f"Leetcode Question: Please walk through question {problem_dict[random.randint(1, 50)]}")
+
 
 if __name__ == "__main__":
     main()

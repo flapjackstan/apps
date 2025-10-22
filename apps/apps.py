2de6e0151d1779a0c3e1077202e079d8dc677aaa
flapjackstan <elm.s.camargo@gmail.com>
2025-10-22 08:27:50 -0700

    working packaging

--- apps/apps.py
+++ apps/apps.py
@@ -1,14 +1,14 @@
 """Main Typer CLI that groups all date-related tools."""
 
 import typer
-import holiday
-import dates
+import apps.holiday
+import apps.dates
 
 app = typer.Typer(help="Collection of date utilities: date math and holiday info.")
 
 # Register subcommands
-app.add_typer(dates.app, name="dates", help="Work with date calculations.")
-app.add_typer(holiday.app, name="holiday", help="View holiday information.")
+app.add_typer(apps.dates.app, name="dates", help="Work with date calculations.")
+app.add_typer(apps.holiday.app, name="holiday", help="View holiday information.")
 
 
 def main():

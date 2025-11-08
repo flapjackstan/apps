368f9ea2db7e46d8a469c57a49bb2a8c8b572c99
flapjackstan <elm.s.camargo@gmail.com>
2025-11-08 12:35:27 -0800

    changed imports

--- apps/apps.py
+++ apps/apps.py
@@ -1,21 +1,21 @@
 """Main Typer CLI that groups all date-related tools."""
 
 import typer
-import apps.holiday
-import apps.dates
-import apps.names
+from apps.holiday import app as holiday_app
+from apps.dates import app as dates_app
+from apps.names import app as names_app
 
-app = typer.Typer(help="Collection of date utilities: date math and holiday info.")
+cli = typer.Typer(help="Collection of date utilities: date math and holiday info.")
 
 # Register subcommands
-app.add_typer(apps.dates.app, name="dates", help="Work with date calculations.")
-app.add_typer(apps.holiday.app, name="holiday", help="View holiday information.")
-app.add_typer(apps.names.app, name="names", help="Generate n random names from character pool")
+cli.add_typer(dates_app, name="dates", help="Work with date calculations.")
+cli.add_typer(holiday_app, name="holiday", help="View holiday information.")
+cli.add_typer(names_app, name="names", help="Generate n random names from character pool")
 
 
 def main():
     """CLI entrypoint."""
-    app()
+    cli()
 
 
 if __name__ == "__main__":
